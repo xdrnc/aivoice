217895c45c45e683ff2b6d099789cea99db12f57
Alexander <alexand312z@hotmail.com>
2026-02-05 12:21:59 -0500

    init front react base

--- VoiceAgent/Program.cs
+++ VoiceAgent/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSignalR();
+
 
 var app = builder.Build();
 
@@ -33,6 +35,7 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+app.MapHub<VoiceHub>("/voiceHub");
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
